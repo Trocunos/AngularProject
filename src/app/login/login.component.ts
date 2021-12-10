83c402495a781bb68d86a2454dd6e65ce74028ad
VINICIUS CUNHA <vinicius.cunha5@fatec.sp.gov.br>
2021-12-10 13:27:56 -0300

    fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,53 +5,44 @@ import { LoginUsuarioService } from './login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-
-
 export class LoginComponent implements OnInit {
+  login = {
+    email: '',
+    password: '',
+  };
 
-
-  login={
-    email: "",
-    password: "",
-
-  }
-  
-
-  constructor(private router: Router, private readonly loginUsuarioService: LoginUsuarioService) { }
+  constructor(
+    private router: Router,
+    private readonly loginUsuarioService: LoginUsuarioService,
+  ) {}
 
   ngOnInit(): void {
     //if(localStorage.getItem("user")){
-      const userStorage: any=JSON.parse(localStorage.getItem("user") || "{}")
-      this.login.email=userStorage.email
-   // }
+    const userStorage: any = JSON.parse(localStorage.getItem('user') || '{}');
+    this.login.email = userStorage.email;
+    // }
   }
 
-  
-  loginUsuario(){
-    let data1={
+  loginUsuario() {
+    let data1 = {
       username: this.login.email,
-      password: this.login.password
+      password: this.login.password,
     };
-    console.log(this.login)
-    this.loginUsuarioService.loginUsuario(data1).subscribe((data:any)=>{
-      alert("Login realizado com sucesso")
-      localStorage.setItem("user", JSON.stringify(data.body))
-      this.router.navigate(["updateaccount"])
-    }, error => {
-      alert(`${this.login.email} Erro ao logar`)
-    })
+    console.log(this.login);
+    this.loginUsuarioService.loginUsuario(data1).subscribe(
+      (data: any) => {
+        alert('Login realizado com sucesso');
+        localStorage.setItem('user', JSON.stringify(data.body));
+        this.router.navigate(['updateaccount']);
+      },
+      (error) => {
+        alert(`${this.login.email} Erro ao logar`);
+      }
+    );
   }
-  HandleLogin(email: string) {
-
-    this.login.email = email;
-
-
-    this.router.navigate(['/home'], {state: {email}});
-
+  HandleLogin() {
+    this.router.navigate(['/createaccount']);
   }
-
 }
-
-
